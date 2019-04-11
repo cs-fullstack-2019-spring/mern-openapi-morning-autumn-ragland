c6e64ba62d1c2c7577704efe5ae2d99cf95f4ceb
autumn-ragland <raglanda998@gmail.com>
2019-04-11 12:24:26 -0500

    new routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import List from "./components/List";
+import Africa from "./components/Africa";
+import Americas from "./components/Americas";
+import Asia from "./components/Asia";
+import Oceania from "./components/Oceania";
+import Europe from "./components/Europe";
 
 class App extends Component {
 
@@ -18,9 +23,12 @@ class App extends Component {
               <Link className={'linkSpace'} to={'/list/europe'}>Europe</Link>
               <Link className={'linkSpace'} to={'/list/oceania'}>Oceania</Link>
               <h2>Click one of the regions to see all of the countries listed in it</h2>
-              <Route path={'/list/:id'} component={List}/>
+              <Route path={'/list/africa'} component={Africa}/>
+              <Route path={'/list/americas'} component={Americas}/>
+              <Route path={'/list/asia'} component={Asia}/>
+              <Route path={'/list/oceania'} component={Oceania}/>
+              <Route path={'/list/europe'} component={Europe}/>
           </Router>
-
       </div>
     );
   }
---
+++ client/src/components/Asia.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class Asia extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            data:[],
+        };
+    }
+
+    componentDidMount() {
+        console.log(this.props.match.params.id);
+        fetch('https://restcountries.eu/rest/v2/region/asia')
+            .then(data => data.json())
+            .then(dataJSON => this.setState({data:dataJSON}))
+    }
+
+    render() {
+
+        var langauges;
+
+        let mapData = this.state.data.map((eachCountry,i) => {
+
+            let total = 0;
+            for (langauges in eachCountry.languages){
+                total ++
+            }
+
+            return(
+
+                <div key={i}>
+                    <img src={eachCountry.flag} alt='flag'/>
+                    <h2>Name: {eachCountry.name} ({eachCountry.alpha2Code})</h2>
+                    <p>Capital: {eachCountry.capital}</p>
+                    <p>Primary Language:{eachCountry.languages[0].nativeName} </p>
+                    <p>Languages Spoken:{total} </p>
+                </div>
+            )
+        });
+
+        return (
+            <div className="App">
+                <h1>Asia</h1>
+                <div className={'mapGrid'}>
+                    {mapData}
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Asia;
